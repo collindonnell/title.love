67a8e6d3284388d559781fbee6634057febcaffa
Collin Donnell <collin@waveform.fm>
2025-07-07 00:01:25 -0700

    Remove connect

--- app/javascript/controllers/title_case_controller.js
+++ app/javascript/controllers/title_case_controller.js
@@ -4,8 +4,6 @@ import { titleCase } from "title-case"
 // Connects to data-controller="title-case"
 export default class extends Controller {
   static targets = ["input", "output"]
-  connect() {
-  }
 
   updateOutput() {
     const inputValue = this.inputTarget.value
