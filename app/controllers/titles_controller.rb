1cca5b2a7feceb5753100c73b59fe4954357f41c
Collin Donnell <collin@waveform.fm>
2025-07-09 13:21:32 -0700

    Set up CRUD for titles controller

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -2,14 +2,31 @@ class TitlesController < ApplicationController
   before_action :require_login
 
   def index
+    @titles = current_user.titles.order(created_at: :desc)
   end
 
   def edit
+    @title = current_user.titles.find(params[:id])
   end
 
   def create
+    @title = current_user.titles.new(title_params)
+    if @title.save
+      redirect_to titles_path, notice: "Title created successfully."
+    else
+      render :new
+    end
   end
 
   def destroy
+    @title = current_user.titles.find(params[:id])
+    @title.destroy
+    redirect_to titles_path, notice: "Title deleted."
+  end
+
+  private
+
+  def title_params
+    params.require(:title).permit(:title)
   end
 end
