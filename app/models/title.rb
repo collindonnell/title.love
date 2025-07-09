1c200b26da68fc96366f035f147f12135ca1a884
Collin Donnell <collin@waveform.fm>
2025-07-09 13:16:11 -0700

    Add associations

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,4 @@
 class Title < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :titles, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
