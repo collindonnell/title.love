903deb9a3e41b1b05fa8a1c1ad49716743d5d51b
Collin Donnell <collin@waveform.fm>
2025-07-18 23:25:38 -0700

    Show deleted toast with fade

--- app/javascript/controllers/transition_controller.js
+++ app/javascript/controllers/transition_controller.js
@@ -1,19 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = {
-    class: String,
-    delay: { type: Number, default: 0 },
-    duration: { type: Number, default: 300 },
-  }
+  static values = { delay: Number }
+  static classes = ["appear", "vanish"]
 
   connect() {
-    // Apply animation class after delay
+    this.element.addEventListener("transistioned", () => {
+      console.log("transition complete")
+    })
+
+    this.element.classList.add(...this.vanishClasses)
+    this.element.classList.add(...this.appearClasses)
+
     setTimeout(() => {
-      if (this.hasClassValue) {
-        this.element.classList.add(this.classValue)
-      }
-      setTimeout(() => this.element.remove(), this.durationValue)
+      this.element.classList.remove(...this.appearClasses)
     }, this.delayValue)
   }
 }
