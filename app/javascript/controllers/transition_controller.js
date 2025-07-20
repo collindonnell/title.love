2866cb3e0f1dc771c64c1503497b318876aa6fc9
Collin Donnell <collin@waveform.fm>
2025-07-20 14:15:25 -0700

    Refactor transition

--- app/javascript/controllers/transition_controller.js
+++ app/javascript/controllers/transition_controller.js
@@ -1,19 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { delay: Number }
+  static values = { delay: { Number, default: 800 } }
   static classes = ["appear", "vanish"]
 
   connect() {
-    this.element.addEventListener("transistioned", () => {
+    this.element.addEventListener("transitionend", () => {
       console.log("transition complete")
     })
 
-    this.element.classList.add(...this.vanishClasses)
-    this.element.classList.add(...this.appearClasses)
+    this.element.classList.add(...this.defaultVanish())
+    this.element.classList.add(...this.defaultAppear())
 
     setTimeout(() => {
-      this.element.classList.remove(...this.appearClasses)
+      this.element.classList.remove(...this.defaultAppear())
     }, this.delayValue)
+  }
+
+  defaultVanish() {
+    return ["invisible", "opacity-0", "transition", "delay-150", "duration-500", "ease-in-out"] 
+  }
+
+  defaultAppear() {
+    return ["visible", "opacity-100"]
   }
 }
